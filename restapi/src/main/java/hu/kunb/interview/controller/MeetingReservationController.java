319e5d1ef357d52f86d2f3f01342b3a44c5282db
kunb <balazs.kun@idomsoft.hu>
2023-02-14 21:56:12 +0100

    initial import

---
+++ restapi/src/main/java/hu/kunb/interview/controller/MeetingReservationController.java
@@ -0,0 +1,20 @@
+package hu.kunb.interview.controller;
+
+import hu.kunb.interview.restapi.spring.api.MeetingReservationApi;
+import hu.kunb.interview.restapi.spring.model.MeetingReservationRequestDto;
+import hu.kunb.interview.restapi.spring.model.MeetingReservationResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MeetingReservationController implements MeetingReservationApi {
+
+    @Override
+    @PostMapping("/private/meeting/reservation")
+    public ResponseEntity<MeetingReservationResponseDto> posteetingReservation(MeetingReservationRequestDto meetingReservationRequestDto) {
+        return MeetingReservationApi.super.posteetingReservation(meetingReservationRequestDto);
+    }
+}
