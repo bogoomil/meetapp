ebcc1a0faceec2d7c005518c5ecb8c816d5a347b
kunb <balazs.kun@idomsoft.hu>
2023-02-15 05:19:35 +0100

    gitignore + tests

--- core/src/test/java/hu/kunb/meetingapp/modell/MeetingReservationTests.java
+++ core/src/test/java/hu/kunb/meetingapp/modell/MeetingReservationTests.java
@@ -5,26 +5,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MeetingReservationTests {
 
-    public static final String TO_STRING = "MeetingReservation{day=+999999999-12-31, startHour=1, startMinute=30, endHour=2, endMinute=0}";
+    public static final String USER_NAME = "USER_NAME";
     private MeetingReservation meetingReservation;
+
+    private LocalDateTime start = LocalDateTime.of(2023,1,1,9,0);
+    private LocalDateTime end = LocalDateTime.of(2023,1,1,9,30);
 
     @BeforeEach
     void setUp(){
-        meetingReservation = new MeetingReservation();
-        meetingReservation.day = LocalDate.MAX;
-        meetingReservation.startHour = 1;
-        meetingReservation.startMinute = 30;
-        meetingReservation.endHour = 2;
-        meetingReservation.endMinute = 0;
+        meetingReservation = new MeetingReservation(start,end, USER_NAME);
     }
 
     @Test
     void toStringTest(){
-        assertEquals(TO_STRING, meetingReservation.toString());
+        assertEquals(USER_NAME, meetingReservation.getUserName());
+        assertEquals(start, meetingReservation.getStartTime());
+        assertEquals(end, meetingReservation.getEndTime());
     }
 }
